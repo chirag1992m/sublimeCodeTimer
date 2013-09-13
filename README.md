276ec9d80fe3c0b1900382006f45eb16dc4ac1c9
chirag1992m <chirag1992m.cc@gmail.com>
2013-09-13 22:20:58 +0530

    Added the countdown timer feature with fixed timer values. Added readme

---
+++ README.md
@@ -0,0 +1,17 @@
+#Sublime Text 2/3 Code Timer
+
+##What it does
+It starts a clock in the status bar of sublime text for you to see how much time you are taking to write a code. Helpful in competetive programming. Also, you can set a countdown timer which pops up a message when the timer ends.
+
+##How to use
+1. Download the package
+2. Open sublime text
+3. Go to Preference > Browse Packages
+4. Copy and Paste the plugin directory in the "packages" directory.
+5. Restart sublime text
+6. Now use the keyboard shortcuts or context menu to use the plugin.
+
+##Key bindings
+1. ctrl + shift + x -> start the clock
+2. ctrl + shift + c -> start the countdown timer
+3. ctrl + shift + q -> stop the timer
--- timer.py
+++ timer.py
@@ -1,6 +1,56 @@
 from datetime import datetime, timedelta
 import sublime
-from sublime_plugin import ApplicationCommand
+from sublime_plugin import ApplicationCommand, TextCommand
+
+class CodeCountdownStartCommand(TextCommand):
+	TimeValues = ["10 minutes", "15 minutes", "30 minues", "45 minutes", "1 hour"]
+	TimeValuesInSeconds = {
+		0: 600,
+		1: 900,
+		2: 1800,
+		3: 2700,
+		4: 3600
+	}
+
+	def run(self, edit):
+		if not hasattr(sublime, 'code_timer_isrunning'):
+			sublime.code_timer_isrunning = True
+		elif sublime.code_timer_isrunning is True:
+			sublime.error_message('Code Timer is already running, stop previous first.')
+			return
+		else:
+			sublime.code_timer_isrunning = True
+
+		if sublime.code_timer_isrunning is True:
+			self.view.window().show_quick_panel(CodeCountdownStartCommand.TimeValues,
+				CodeCountdownStartCommand.onChoose,
+				sublime.MONOSPACE_FONT)
+
+	@staticmethod
+	def onChoose(choice):
+		if choice == -1:
+			sublime.code_timer_isrunning = False
+		else:
+			CodeCountdownStartCommand.decrementTimer(datetime.now(), timedelta(seconds = CodeCountdownStartCommand.TimeValuesInSeconds[choice]))
+
+	@staticmethod
+	def decrementTimer(startTime, countDownTime):
+		if sublime.code_timer_isrunning is True:
+			timeLeft = (startTime - datetime.now()) + countDownTime
+			# sublime.status_message("time: {0}".format(timeLeft.total_seconds()))
+			if timeLeft.total_seconds() > 0:
+				CodeCountdownStartCommand.printStats(timeLeft)
+				sublime.set_timeout(lambda: CodeCountdownStartCommand.decrementTimer(startTime, countDownTime),
+					100)
+			else:
+				sublime.code_timer_isrunning = False
+				sublime.message_dialog("Your time is up!")
+
+	@staticmethod
+	def printStats(timeElapsed):
+		seconds = timeElapsed.seconds
+		minutes, seconds = divmod(seconds, 60)
+		sublime.status_message("Time Left, {0}:{1}".format(minutes, seconds))
 
 class CodeTimerStartCommand(ApplicationCommand):
 	def run(self):
